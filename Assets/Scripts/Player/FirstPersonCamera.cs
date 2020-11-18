d01c8e45b7743e16e7b45638a138479f0c18d772
sbuitenkamp <buitenkamp.developer@gmail.com>
2020-11-18 15:13:07 +0100

    initial commit

---
+++ Assets/Scripts/Player/FirstPersonCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float MouseSensitivity = 3f;
+    public Transform PlayerBody;
+
+    private float XRotation = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public float Speed = 12f;
+    public float Gravity = -9.81f;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    private Vector3 Velocity;
+    private bool IsGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (IsGrounded && Velocity.y < 0) Velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Transform transform1 = transform;
+        Vector3 move = transform1.right * x + transform1.forward * z;
+
+        Controller.Move(Speed * Time.deltaTime * move);
+        Velocity.y += Gravity * Time.deltaTime;
+        Controller.Move(Velocity * Time.deltaTime);
+    }
+}
